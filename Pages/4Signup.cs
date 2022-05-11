3675b41c1bea398643e4a4c5a07039c0279a6282
Pratik Kabade <pratikkabade@hotmail.com>
2022-05-11 16:06:51 +0530

    Visual Changes
    Changes to Form and Available Rooms using CSS

---
+++ Pages/4Signup.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+
+namespace HomeStay.Pages
+{
+    public class SignupModel : PageModel
+    {
+        private readonly ILogger<SignupModel> _logger;
+
+        public SignupModel(ILogger<SignupModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+        }
+    }
+}
