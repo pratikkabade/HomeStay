125e7c481730a604bd5d5e7477eda372c65f962f
Pratik Kabade <pratikkabade@hotmail.com>
2022-05-11 15:59:05 +0530

    Created Files
    Using VS Code

---
+++ Pages/3Signin.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+
+namespace HomeStay.Pages
+{
+    public class SigninModel : PageModel
+    {
+        private readonly ILogger<SigninModel> _logger;
+
+        public SigninModel(ILogger<SigninModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+        }
+    }
+}
